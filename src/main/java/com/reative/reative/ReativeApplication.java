19f45557eae7b218529247e38bea8494e29771d5
mehmet <memojja1907@gmail.com>
2018-03-08 02:53:19 +0300

    initialize project

---
+++ src/main/java/com/reative/reative/ReativeApplication.java
@@ -0,0 +1,12 @@
+package com.reative.reative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/reative/reative/ReativeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reative.reative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
